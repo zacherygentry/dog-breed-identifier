2a9a7ceb26b0c927b25311549a703d46a88fde11
Zachery Gentry <zacherygentry@gmail.com>
2018-12-22 13:18:32 -0600

    init commit

---
+++ Dog Breed Identifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog Breed Identifier
+//
+//  Created by Zachery Gentry on 12/22/18.
+//  Copyright © 2018 com.gentry.adbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Dog Breed IdentifierTests/Dog_Breed_IdentifierTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dog_Breed_IdentifierTests.swift
+//  Dog Breed IdentifierTests
+//
+//  Created by Zachery Gentry on 12/22/18.
+//  Copyright © 2018 com.gentry.adbi. All rights reserved.
+//
+
+import XCTest
+@testable import Dog_Breed_Identifier
+
+class Dog_Breed_IdentifierTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
